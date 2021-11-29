571fee3411b7c35740a575594f65d28d2da3bc74
Anto8819 <mrap1988@gmail.com>
2021-11-29 20:56:22 -0300

    change store to avoid error with redux dev tools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
-import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
     rootReducer, 
-    compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    composeEnhancer(applyMiddleware(thunkMiddleware))
     );
 
 export default store;
